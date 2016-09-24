ea66f712a0910e5bf746cf8964275cc82af89dfa
vlymar <victorl@berkeley.edu>
2016-09-23 22:16:00 -0700

    move main out into own file

--- flan.go
+++ flan.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 )
 
@@ -46,25 +45,4 @@ func flanpage(arg string) error {
 func flannotate() error {
 	fmt.Println("flannotating lol")
 	return nil
-}
-
-func main() {
-	var err error = nil
-
-	args := os.Args[1:]
-
-	switch len(args) {
-	case 0:
-		err = flannotate()
-	case 1:
-		err = flanpage(args[0])
-	default:
-		log.Fatal("Pass 1 or 0 args")
-	}
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	os.Exit(0)
 }
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	var err error = nil
+
+	args := os.Args[1:]
+
+	switch len(args) {
+	case 0:
+		err = flannotate()
+	case 1:
+		err = flanpage(args[0])
+	default:
+		log.Fatal("Pass 1 or 0 args")
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(0)
+}
