0e0e24dab7356e4d4dd4a8cdb19281de90555c01
vlymar <victorl@berkeley.edu>
2016-09-23 22:11:47 -0700

    minor refactoring, also excplicitly close less input pipe or else things go bad

--- flan.go
+++ flan.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -14,33 +15,54 @@ $ flan lsof
 lsof -i :$port
 */
 
-func main() {
-	args := os.Args[1:]
-
-	if len(args) == 0 {
-		log.Fatal("no args passed")
-	}
-
-	manOut, err := manOutput(args[0], os.Stderr)
+func flanpage(arg string) error {
+	manOut, err := manOutput(arg, os.Stderr)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	lessCmd := lessCommand(os.Environ(), os.Stdout, os.Stderr)
 
 	lessIn, err := lessCmd.StdinPipe()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	if err := lessCmd.Start(); err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	lessIn.Write([]byte("future flannotation...\n\n"))
 	lessIn.Write(manOut)
+	lessIn.Close()
 
 	if err := lessCmd.Wait(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func flannotate() error {
+	fmt.Println("flannotating lol")
+	return nil
+}
+
+func main() {
+	var err error = nil
+
+	args := os.Args[1:]
+
+	switch len(args) {
+	case 0:
+		err = flannotate()
+	case 1:
+		err = flanpage(args[0])
+	default:
+		log.Fatal("Pass 1 or 0 args")
+	}
+
+	if err != nil {
 		log.Fatal(err)
 	}
 
